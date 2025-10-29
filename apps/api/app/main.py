f8520e1fdda0f4b93291d232eda526fccc9adaf6
GabriellBarbosa <bielbarbosa900@gmail.com>
2025-10-29 15:37:41 -0300

    feat: Initial config

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -11,7 +11,6 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
